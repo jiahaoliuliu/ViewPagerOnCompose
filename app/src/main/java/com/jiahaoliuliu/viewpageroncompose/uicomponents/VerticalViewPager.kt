0d024185994eee0570cb320e3f59de7faf0b36aa
Jiahao Liu <jiahaoliuliu@gmail.com>
2024-07-14 18:30:31 +0200

    JL Settled the listener for the current page

--- app/src/main/java/com/jiahaoliuliu/viewpageroncompose/uicomponents/VerticalViewPager.kt
+++ app/src/main/java/com/jiahaoliuliu/viewpageroncompose/uicomponents/VerticalViewPager.kt
@@ -1,5 +1,6 @@
 package com.jiahaoliuliu.viewpageroncompose.uicomponents
 
+import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -10,7 +11,9 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,6 +28,25 @@ fun VerticalViewPager(modifier: Modifier = Modifier) {
         val pagerState = rememberPagerState(pageCount = {
             10
         })
+
+        LaunchedEffect(key1 = pagerState) {
+            // Collect from a snapshot flow reading the current page
+            snapshotFlow { pagerState.currentPage }.collect {
+                page ->
+                // Do something with each page change, for example
+                // viewModel.sendPageSelectedEvent(page)
+                Log.d("Page change", "Current page changed to $page")
+            }
+
+            // Collect from a snapshot flow reading the current page
+            snapshotFlow { pagerState.settledPage }.collect {
+                    page ->
+                // Do something with each page change, for example
+                // viewModel.sendPageSelectedEvent(page)
+                Log.d("Page change", "Settle page changed to $page")
+            }
+        }
+
         VerticalPager(state = pagerState) { page ->
             // Our page content
             Text(
@@ -41,7 +63,8 @@ fun VerticalViewPager(modifier: Modifier = Modifier) {
         Button(onClick = {
             coroutineScope.launch {
                 // Call scroll to on pagerState
-                pagerState.scrollToPage(5)
+//                pagerState.scrollToPage(5)
+                pagerState.animateScrollToPage(5)
             }
         }, modifier = Modifier.align(Alignment.BottomCenter)) {
             Text(text = "Jump to Page 5")
